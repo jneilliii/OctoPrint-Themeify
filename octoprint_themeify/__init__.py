a7332cce233b8188a9f4f0f8cf4cd0944674ee08
jneilliii <jneilliii+github@gmail.com>
2017-12-10 06:02:01 -0500

    POC

--- octoprint_themeify/__init__.py
+++ octoprint_themeify/__init__.py
@@ -15,7 +15,7 @@ class ThemeifyPlugin(octoprint.plugin.StartupPlugin,
     def get_assets(self):
         return dict(
             less=["less/base.less"],
-            css=["dist/themeify.min.css","css/includes.css","themes/%s.css" % self._settings.get(["theme"])],
+            css=["css/includes.css","themes/%s.css" % self._settings.get(["theme"])],
             js=["dist/themeify.min.js"]
         )
 
